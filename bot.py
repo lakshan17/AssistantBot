36d2f1820542bb4e7e8c8e3625a10fa667b0662a
Tʜᴇɴᴜᴋ Cʜᴀɴᴜᴋᴀ Dᴇᴠɪɴᴅᴀ <94178410+DevindaOfficial@users.noreply.github.com>
2022-03-20 11:02:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,9 +66,9 @@ BACK = InlineKeyboardMarkup(
             ]
         )
 
-LOG_TEXT = "<b>✅New</b>\n\nID: <code>{}</code>\nFirst Name: <a href='tg://user?id={}'>{}{}</a>\nDC ID: <code>{}</code>"
-IF_TEXT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
-IF_CONTENT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
+LOG_TEXT = "<b>✅New User</b>\n\nID: <code>{}</code>\nFirst Name: <a href='tg://user?id={}'>{}{}</a>\nDC ID: <code>{}</code>"
+IF_TEXT = "🍀</u><b>New message</b></u>\n\n<b>🌷Message from:</b> {}\n<b>🌷Name:</b> {}\n\n{}"
+IF_CONTENT = "🍀</u><b>New message</b></u>\n\n<b>🌷Message from:</b> {} \n<b>🌷Name:</b> {}"
 
 
 async def bot_msg():
@@ -130,6 +130,75 @@ async def aboutenu(_, query: CallbackQuery):
         reply_markup=BACK,
      disable_web_page_preview=True
     )      
+
+@bot.on_message(filters.private & filters.text)
+async def pm_text(bot, message):
+    if message.from_user.id == owner_id:
+        await reply_text(bot, message)
+        return
+    info = await bot.get_users(user_ids=message.from_user.id)
+    reference_id = int(message.chat.id)
+    await bot.send_message(
+        chat_id=owner_id,
+        text=IF_TEXT.format(reference_id, info.first_name, message.text),
+        parse_mode="html"
+    )
+
+
+@bot.on_message(filters.private & filters.media)
+async def pm_media(bot, message):
+    if message.from_user.id == owner_id:
+        await replay_media(bot, message)
+        return
+    info = await bot.get_users(user_ids=message.from_user.id)
+    reference_id = int(message.chat.id)
+    await bot.copy_message(
+        chat_id=owner_id,
+        from_chat_id=message.chat.id,
+        message_id=message.message_id,
+        caption=IF_CONTENT.format(reference_id, info.first_name),
+        parse_mode="html"
+    )
+
+
+@bot.on_message(filters.user(owner_id) & filters.text)
+async def reply_text(bot, message):
+    reference_id = True
+    if message.reply_to_message is not None:
+        file = message.reply_to_message
+        try:
+            reference_id = file.text.split()[2]
+        except Exception:
+            pass
+        try:
+            reference_id = file.caption.split()[2]
+        except Exception:
+            pass
+        await bot.send_message(
+            text=message.text,
+            chat_id=int(reference_id)
+        )
+
+
+@bot.on_message(filters.user(owner_id) & filters.media)
+async def replay_media(bot, message):
+    reference_id = True
+    if message.reply_to_message is not None:
+        file = message.reply_to_message
+        try:
+            reference_id = file.text.split()[2]
+        except Exception:
+            pass
+        try:
+            reference_id = file.caption.split()[2]
+        except Exception:
+            pass
+        await bot.copy_message(
+            chat_id=int(reference_id),
+            from_chat_id=message.chat.id,
+            message_id=message.message_id,
+            parse_mode="html"
+        )
 
 
                            
