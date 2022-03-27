6991ab0d61330caace0d3390fad1c2f918e15306
Tʜᴇɴᴜᴋ Cʜᴀɴᴜᴋᴀ Dᴇᴠɪɴᴅᴀ <94178410+DevindaOfficial@users.noreply.github.com>
2022-03-27 10:48:10 +0530

    Update __init__.py

--- assistant/plugins/__init__.py
+++ assistant/plugins/__init__.py
@@ -1,7 +1,7 @@
 from os.path import dirname, basename, isfile, join
 import glob
 
-plugins = glob.glob(join(dirname(__file__), "*.py"))
+modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [
-    basename(f)[:-3] for f in plugins if isfile(f) and not f.endswith("__init__.py")
+    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
 ]
