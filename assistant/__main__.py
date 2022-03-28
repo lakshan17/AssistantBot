d4b0d93defb36089769b1c3d866809fb88ed182a
Tʜᴇɴᴜᴋ Cʜᴀɴᴜᴋᴀ Dᴇᴠɪɴᴅᴀ <94178410+DevindaOfficial@users.noreply.github.com>
2022-03-28 18:57:32 +0530

    Update __main__.py

--- assistant/__main__.py
+++ assistant/__main__.py
@@ -11,7 +11,7 @@ assistant = Client(
    bot_token=C.TG_BOT_TOKEN,
 )
 
-owner_id=C.OWNER_ID
+ADMIN=C.OWNER_ID
 
 IMAGE="""https://telegra.ph/file/e97f50bc4e0920f0c2475.jpg"""
 
@@ -203,6 +203,46 @@ async def devmenu(_, query: CallbackQuery):
         reply_markup=DEV_BTN,
      disable_web_page_preview=True
     )    
+
+
+@assistant.on_message(filters.command('submit') & filters.private)
+async def report(bot, message):
+        if message.reply_to_message:
+                                  await bot.send_message(chat_id=ADMIN, text=f"<b>⭕️NEW MESSAGE⭕️\n \n🧿 Name: {message.from_user.mention}\n🧿 User ID:</b> <code>{message.chat.id}</code>")
+                                  await bot.forward_messages(chat_id=ADMIN, from_chat_id=message.from_user.id, message_ids=message.reply_to_message.message_id)
+                                  await message.reply_text("<b>✅ Your Feedback Successfully Submitted to the Admins</b>")
+        else:
+             await message.reply_text("<b>Use this command as the reply of any Message to Report</b>")
+
+                         
+        
+@assistant.on_message(filters.command('reply') & filters.private)
+async def replyt(bot, message):
+    if message.from_user.id == ADMIN: 
+               if message.reply_to_message:
+                                    userid=int(message.text.replace("/reply"," "))
+                                    await bot.send_message(chat_id=userid, text=f"<b>An Admin is responded to your feedback ✨</b>")
+                                    await bot.copy_message(chat_id=userid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
+                                    await message.reply_text("<b>✅ Your Reply Successfully Send to the User</b>")
+               else:
+                    await message.reply_text("<b>Use this command as the reply of any Message to Reply</b>")                         
+    else:
+         await message.reply_text("<b>That's not for you bruh 😅</b>")
+
+
+                          
+@assistant.on_message(filters.command('send') & filters.private)
+async def send(bot, message):
+    if message.from_user.id == ADMIN: 
+               if message.reply_to_message:
+                                    chatid=int(message.text.replace("/send"," "))
+                                    await bot.copy_message(chat_id=chatid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
+                                    await message.reply_text("<b>✅ Message Successfully Send to the Group</b>")
+               else:
+                    await message.reply_text("<b>Use this command as the reply of any Message to Send in Group</b>")                         
+    else:
+         await message.reply_text("<b>That's not for you bruh 😅</b>")
+
 
 @assistant.on_message(filters.command("mlogo"))
 async def logomake(bot, update):
