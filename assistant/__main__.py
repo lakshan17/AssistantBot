b676481952818ee69830d055417bb141d8a3894e
Lakshan Pathum <107621003+lakshan17@users.noreply.github.com>
2022-10-10 22:36:39 +0530

    Update __main__.py

--- assistant/__main__.py
+++ assistant/__main__.py
@@ -209,7 +209,7 @@ async def devmenu(_, query: CallbackQuery):
 async def report(bot, message):
         if message.reply_to_message:
                                   await bot.send_message(chat_id=ADMIN, text=f"<b>⭕️NEW MESSAGE⭕️\n \n🧿 Name: {message.from_user.mention}\n🧿 User ID:</b> <code>{message.chat.id}</code>")
-                                  await bot.forward_messages(chat_id=ADMIN, from_chat_id=message.from_user.id, message_ids=message.reply_to_message.message_id)
+                                  await bot.forward_messages(chat_id=ADMIN, from_chat_id=message.from_user.id, message_ids=message.reply_to_message.chat_id)
                                   await message.reply_text("<b>✅ Your Feedback Successfully Submitted to the Admins</b>")
         else:
              await message.reply_text("<b>Use this command as the reply of any Message to Report</b>")
